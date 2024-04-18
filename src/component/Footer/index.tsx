2071af7140ab297ee5e6602ab9ebd4f114da7296
Shawen Aldred Harker <harkershawen@gmail.com>
2024-04-18 20:02:43 +0200

    Update index.tsx

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -14,6 +14,7 @@ export default function Footer () {
         >
           Coinlore
         </Link>{" "} and is only used as mock data for training purposes.
+        The loading of the web page may take a bit longer than expected as it is hosted on a free service. This purely used to showcase my skills using data from an API and integrating it into a web app.
       </Text>
     </Container>
   );
